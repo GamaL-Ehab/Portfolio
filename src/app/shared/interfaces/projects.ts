d03921e278ad7a49bb7ab6bdbe101547689df034
Gamal-Ehab <gamalehabg@gmail.com>
2025-06-26 03:55:52 +0300

    Adding Projects

---
+++ src/app/shared/interfaces/projects.ts
@@ -0,0 +1,10 @@
+export interface Projects {
+    title: string,
+    description: string,
+    thumbnail: string,
+    repo: string,
+    liveDemo: string,
+    date: string,
+    techs: string[],
+    realProject: boolean
+}
